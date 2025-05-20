011161a827bafbd51373397aca422be557331a0c
Ajeet238 <ajeetkumar23898@gmail.com>
2025-05-20 11:50:37 +0530

    port 8083 killed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ get_port() {
   case "$1" in
     userservice) echo 8081 ;;
     filestorage) echo 8082 ;;
-    filemetadata) echo 8083 ;;
+    filemetadata) echo 8088 ;;
     giftservice) echo 8084 ;;
     postapi) echo 8087 ;;
     apigateway) echo 9090 ;;
